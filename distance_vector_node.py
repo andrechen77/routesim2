eeb4d6318825ba0c950f668d77531d8c8a3eb33b
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2024-02-27 14:56:43 -0600

    fixes for dv algorithm

--- distance_vector_node.py
+++ distance_vector_node.py
@@ -8,7 +8,9 @@ class Distance_Vector_Node(Node):
         # the latest time our DV was updated
         self.latest_dv_update = -1
         # maps from destination node id to (time_cost, path_to_dest) where
-        # path_to_dest does not include self, but does include the destination
+        # path_to_dest represents the series of next_hops required to get to the
+        # destination. thus, it never includes self.id, and is empty when this
+        # node is advertising itself in its DV.
         self.distance_vector = {}
 
         # maps from a neighbor id to a tuple (timestamp, DV) where DV is the
@@ -27,17 +29,15 @@ class Distance_Vector_Node(Node):
     # Returns whether the DV was changed
     def recalculate_dv(self):
         # print("recalculating dv")
-        # start by adding myself and my direct neighbors to new_dv
-        new_dv = {neighbor: (latency, [neighbor]) for neighbor, latency in self.direct_links.items()}
-        new_dv[self.id]=(0, [self.id])
+        # start by adding myself to new_dv
+        new_dv = {self.id: (0, [])}
         # loop through neighbor's dv's to look for best paths to all dsts
         for neighbor, (_, neighbor_dv) in self.latest_neighbor_dvs.items():
             for dest, (time_cost, path) in neighbor_dv.items():
+                # print(f"possible path to {dest} with through neighbor {neighbor} and path {path} with cost {time_cost}")
                 cost = self.direct_links[neighbor] + time_cost
-                if dest not in new_dv or cost < new_dv[dest][0]:
-                    # make sure path has no repeat nodes (avoids cycles)
-                    if neighbor not in path and self.id not in path:
-                        new_dv[dest] = (cost, [neighbor] + path)
+                if (dest not in new_dv or cost < new_dv[dest][0]) and self.id not in path:
+                    new_dv[dest] = (cost, [neighbor] + path)
         changed = self.distance_vector != new_dv
         self.distance_vector = new_dv
         self.latest_dv_update = self.get_time()
@@ -46,24 +46,24 @@ class Distance_Vector_Node(Node):
     def link_has_been_updated(self, neighbor, latency):
         # print(f"\ntime: {self.get_time()}, node: {self.id}, link has been updated: nei={neighbor} cost={latency}")
         # latency = -1 if delete a link
-        changed = False
         if latency == -1 and neighbor in self.neighbors:
             # print(f"deleting neighbor {neighbor}")
             del self.direct_links[neighbor]
             del self.latest_neighbor_dvs[neighbor]
             self.neighbors.remove(neighbor)
-            changed=True
         elif latency != -1 and neighbor not in self.neighbors:
             # print(f"adding neighbor {neighbor}"")
             self.direct_links[neighbor] = latency
-            self.latest_neighbor_dvs[neighbor] = (-1, {})
+            self.latest_neighbor_dvs[neighbor] = (-1, {neighbor: (latency, [neighbor])})
+            # instead of depending on the neighbor to advertise their existence
+            # to us we know that we already have a path to them so just assume
+            # that we can use it
             self.neighbors.append(neighbor)
-            changed=True
         elif latency != -1 and neighbor in self.neighbors:
             self.direct_links[neighbor]=latency
 
         # if DV changed, notify neighbors
-        if self.recalculate_dv() or changed:
+        if self.recalculate_dv():
             # print("dv changed, notifying neighbors")
             # print(self)
             self.send_to_neighbors(self.serialize_routing_message())
@@ -72,12 +72,12 @@ class Distance_Vector_Node(Node):
     def process_incoming_routing_message(self, m):
         # print(f"\ntime: {self.get_time()}, node: {self.id}, processing incoming routing message:", m)
         sender_id, new_timestamp, new_dv = self.deserialize_routing_message(m)
-        # our neighbor died after sending this message but before we received the message
         if sender_id not in self.neighbors:
+            # our neighbor died after sending this message but before we received the message
             # print("received message from dead neighbor, discarding")
             return
         else:
-            current_timestamp, _ = self.latest_neighbor_dvs.get(sender_id, (-1, {}))
+            current_timestamp, _ = self.latest_neighbor_dvs[sender_id]
             if new_timestamp <= current_timestamp:
                 # print("received old message, discarding")
                 return
